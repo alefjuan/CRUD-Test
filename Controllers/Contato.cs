82cfb68b44c720cf1d9481f8b1605c13aa7e57d4
Alef Juan <alefjuan88@gmail.com>
2023-02-24 11:42:51 -0300

    first commit

---
+++ Controllers/Contato.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControleDeContatos.Controllers
+{
+    public class Contato : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Criar()
+        {
+            return View();
+        }
+        public IActionResult Editar()
+        {
+            return View();
+        }
+        public IActionResult ApagarConfirmacao()
+        {
+            return View();
+        }
+    }
+}
