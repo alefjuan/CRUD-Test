4f5b603dddb718f715a3aaa9e711517ea0288ccf
Alef Juan <alefjuan88@gmail.com>
2023-03-01 10:30:06 -0300

    add delete and update

--- Repositorio/ContatoRepositorio.cs
+++ Repositorio/ContatoRepositorio.cs
@@ -1,4 +1,5 @@
-﻿using ControleDeContatos.Data;
+﻿using ControleDeContatos.Controllers;
+using ControleDeContatos.Data;
 using ControleDeContatos.Models;
 
 namespace ControleDeContatos.Repositorio
@@ -6,10 +7,17 @@ namespace ControleDeContatos.Repositorio
     public class ContatoRepositorio : IContatoRepositorio
     {
         private readonly BancoContext _bancoContext;
+        private object _context;
+
         public ContatoRepositorio(BancoContext bancoContext) 
         {
-            _bancoContext = bancoContext;    
+           this._bancoContext = bancoContext;    
         }
+        public ContatoModel ListarPorId(int id)
+        {
+            return _bancoContext.Contatos.FirstOrDefault(x=>x.Id==id);
+        }
+
         public List<ContatoModel> BuscarTodos()
         {
             return _bancoContext.Contatos.ToList();
@@ -21,6 +29,31 @@ namespace ControleDeContatos.Repositorio
             return contato;
         }
 
-      
+        public ContatoModel Atualizar(ContatoModel contato)
+        {
+            ContatoModel contatoDB = ListarPorId(contato.Id);
+
+            if (contatoDB == null) throw new Exception("Erro na atualização do contato");
+            
+                contatoDB.Nome = contato.Nome;
+                contatoDB.Email = contato.Email;
+                contatoDB.Celular = contato.Celular;
+            _bancoContext.Contatos.Update(contatoDB);
+            _bancoContext.SaveChanges();
+
+            return contatoDB;
+        }
+
+        public bool Apagar(int id)
+        {
+            ContatoModel contatoDB = ListarPorId(id);
+
+            if (contatoDB == null) throw new Exception("Erro na exclusão do contato");
+
+            _bancoContext.Contatos.Remove(contatoDB);
+            _bancoContext.SaveChanges();
+
+            return true;
+        }
     }
 }
